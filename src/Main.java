fecf32101db9732d2aa851c0ec4213229b752a60
TengJo <tengjh@mail.uc.edu>
2022-02-13 21:06:22 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String affiliation;
+        affiliation = "r";
+
+            if (affiliation.equalsIgnoreCase("R"))
+                {
+                    System.out.println("You get a Republican Elephant.");
+                }
+            else if (affiliation.equalsIgnoreCase("D"))
+                {
+                    System.out.println("You get a Democratic Donkey.");
+                }
+            else if (affiliation.equalsIgnoreCase("I"))
+                {
+                    System.out.println("You're an Independent Man.");
+                }
+            else
+                {
+                    System.out.println("I don't know what you are!");
+                }
+
+
+
+    }
+}
