0f5b4cf98e75e4502ab80175cf0f498856e55225
TengJo <tengjh@mail.uc.edu>
2022-02-13 21:07:06 -0500

    changed affiliation to d

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main
     public static void main(String[] args)
     {
         String affiliation;
-        affiliation = "r";
+        affiliation = "d";
 
             if (affiliation.equalsIgnoreCase("R"))
                 {
